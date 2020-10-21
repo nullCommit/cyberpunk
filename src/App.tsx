281ca70c861dead65784040dd00f096b6c8642ca
mundaugame <mundaugame@gmail.com>
2020-10-21 17:00:32 -0300

    implemented countdown component, some styling improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { BsTerminal, BsArrowRight } from 'react-icons/bs';
+import { ImEarth, ImQrcode } from 'react-icons/im';
+import { IoMdPlanet, IoMdList, IoMdImages } from 'react-icons/io';
 
 import './styles/global.css';
 import './styles/landing.css';
 
+import Countdown from './components/Countdown';
 import logoImg from './images/cyberpunk-logo.svg';
 
 function App() {
@@ -10,29 +14,94 @@ function App() {
     <div id='page-landing'>
       <div className='content-wrapper'>
         <img src={logoImg} alt='Cyberpunk' />
-        <div className='cowntdown-ontainer'>
+        <div className='cowntdown-container'>
           <h1>Countdown</h1>
-          <h2>00:00:00:00</h2>
+          <Countdown />
         </div>
       </div>
       <div className='grid-container'>
         <div className='grid-1'>
-          <h1>test1</h1>
+          <div className='box-content'>
+            <BsTerminal className='grid-top-icon' />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ea
+              odio aliquam impedit expedita a eius. Quo officia, impedit ad,
+              minus doloribus, nemo velit modi rem fuga expedita at rerum?
+            </p>
+          </div>
         </div>
         <div className='grid-2'>
-          <h1>test2</h1>
+          <div className='box-content'>
+            <ImEarth className='grid-top-icon' />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
+              praesentium quia delectus similique, impedit accusantium, facilis
+              officia rerum aut nisi qui porro cupiditate dolor nobis vitae iure
+              nihil, atque quas.
+            </p>
+            <div className='grid-btn'>
+              <a href='' className='btn'>
+                Look this <BsArrowRight />
+              </a>
+            </div>
+          </div>
         </div>
         <div className='grid-3'>
-          <h1>test3</h1>
+          <div className='box-content'>
+            <ImQrcode className='grid-top-icon' />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia
+              perspiciatis vitae tempora asperiores veritatis ea aliquam harum,
+              impedit enim dolor reiciendis commodi adipisci fugit obcaecati
+              neque magnam totam inventore? Minus!
+            </p>
+            <div className='grid-btn'>
+              <a href='' className='btn'>
+                See more <BsArrowRight />
+              </a>
+            </div>
+          </div>
         </div>
         <div className='grid-4'>
-          <h1>test4</h1>
+          <div className='box-content'>
+            <IoMdImages className='grid-top-icon' />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet
+              iste saepe eaque cupiditate rem atque animi nesciunt similique
+              nostrum minus quisquam dolore omnis tempore asperiores repudiandae
+              quos labore, magni eos?
+            </p>
+            <div className='grid-btn'>
+              <a href='' className='btn'>
+                Go to albums <BsArrowRight />
+              </a>
+            </div>
+          </div>
         </div>
         <div className='grid-5'>
-          <h1>test5</h1>
+          <div className='box-content'>
+            <IoMdPlanet className='grid-top-icon' />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis
+              expedita voluptate id facere accusamus praesentium.
+            </p>
+          </div>
         </div>
         <div className='grid-6'>
-          <h1>test6</h1>
+          <div className='box-content'>
+            <IoMdList className='grid-top-icon' />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+              doloremque illum explicabo ad hic exercitationem dolores cumque
+              ratione cum nihil ipsa, aut ipsum laborum est nam fuga repellat
+              totam at.
+            </p>
+            <div className='grid-btn'>
+              <a href='' className='btn'>
+                Buy <BsArrowRight />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,1 +1,89 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import '../styles/components/countdown.css';
+
+let interval: any;
+
+const Countdown = () => {
+  const [remainingTime, setRemainingTime] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  const [completed, setCompleted] = useState(false);
+
+  const now = new Date();
+  const targetDate = Date.parse('november 19, 2020 11:00:00 AM');
+  const timeLeft = targetDate - now.getTime();
+  const remainingDays = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const remainingHours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+  const remainingMin = Math.floor((timeLeft / (1000 * 60)) % 60);
+  const remainingSeconds = Math.floor((timeLeft / 1000) % 60);
+
+  const formatResult = (num: number) => {
+    if (num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  };
+
+  useEffect(() => {
+    if (completed) {
+      clearInterval(interval);
+    }
+  }, [completed]);
+
+  useEffect(() => {
+    if (!completed) {
+      interval = setInterval(() => {
+        if (timeLeft > 0) {
+          setRemainingTime({
+            days: remainingDays,
+            hours: remainingHours,
+            minutes: remainingMin,
+            seconds: remainingSeconds,
+          });
+        } else {
+          setCompleted(true);
+        }
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [
+    completed,
+    remainingDays,
+    remainingHours,
+    remainingMin,
+    remainingSeconds,
+    timeLeft,
+  ]);
+
+  return (
+    <div id='cowntdown-timer'>
+      <div className='days-content'>
+        <span>Days</span>
+        <h2>{formatResult(remainingTime.days)}</h2>
+      </div>
+      <div className='hours-content'>
+        <span>Hours</span>
+        <h2>{formatResult(remainingTime.hours)}</h2>
+      </div>
+      <div className='minutes-content'>
+        <span>Min</span>
+        <h2>{formatResult(remainingTime.minutes)}</h2>
+      </div>
+      <div className='seconds-content'>
+        <span>Sec</span>
+        <h2>{formatResult(remainingTime.seconds)}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Countdown;
