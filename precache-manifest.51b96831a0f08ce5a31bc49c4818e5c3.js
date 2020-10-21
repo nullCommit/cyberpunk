4945b59265a48753fec63b41ce7b7c4bde441984
mundaugame <mundaugame@gmail.com>
2020-10-21 17:54:15 -0300

    Updates

---
+++ precache-manifest.51b96831a0f08ce5a31bc49c4818e5c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37dea73badb2f0e19715febb85156bca",
+    "url": "/cyberpunk/index.html"
+  },
+  {
+    "revision": "af6e37fe726f53b00954",
+    "url": "/cyberpunk/static/css/main.6766fcfe.chunk.css"
+  },
+  {
+    "revision": "6d3604aad3ad5f03c888",
+    "url": "/cyberpunk/static/js/2.0f73118e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/cyberpunk/static/js/2.0f73118e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6e37fe726f53b00954",
+    "url": "/cyberpunk/static/js/main.29ea1999.chunk.js"
+  },
+  {
+    "revision": "1e4a0439aad51b72b583",
+    "url": "/cyberpunk/static/js/runtime-main.bbfde25d.js"
+  },
+  {
+    "revision": "9a12b5fc2f467088b0512434ac5c3259",
+    "url": "/cyberpunk/static/media/cyberpunk-logo.9a12b5fc.svg"
+  }
+]);
